66428141e67b7a1b6bb2d3ab3490c75e73cf4596
David Li <fantasticmrdavid@gmail.com>
2023-09-02 22:14:07 +1000

    Fix refetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,26 +2,17 @@ import { useQuery } from '@tanstack/react-query'
 import classNames from "classnames"
 import { FlapDisplay, Presets } from 'react-split-flap-effect'
 import styles from "./styles.module.scss"
-import {useEffect, useState} from "react";
 
 export default function Home() {
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [_, setTime] = useState(Date.now());
-
-    useEffect(() => {
-        const interval = setInterval(() => setTime(Date.now()), 1000);
-        return () => {
-            clearInterval(interval);
-        };
-    }, []);
-
   const { data: reading, isLoading } = useQuery(
     ['getReadings'],
     async () =>
       await fetch(`/api/reading`).then((res) =>
         res.json(),
       ),
+      {
+          refetchInterval: 6000,
+      }
   )
 
   if(isLoading || !reading) return <div>loading...</div>
