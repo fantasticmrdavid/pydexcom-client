625e96b51bebf1f71bd98cd949b057bbbb77ff73
David Li <fantasticmrdavid@gmail.com>
2023-09-03 12:07:30 +1000

    Change font, add colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import '../styles/globals.css'
 
-import '@fontsource/orbitron'
+import '@fontsource/roboto'
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
 import classNames from "classnames"
-import { FlapDisplay, Presets } from 'react-split-flap-effect'
 import styles from "./styles.module.scss"
 
 export default function Home() {
@@ -15,10 +14,13 @@ export default function Home() {
       }
   )
 
-  if(isLoading || !reading) return <div>loading...</div>
+  if(isLoading || !reading) return <div className={styles.container}>loading...</div>
 
   const counterClassNames = classNames({
     [styles.container]: true,
+    [styles.bg_green]: reading.mmol_l > 4 && reading.mmol_l < 10,
+    [styles.bg_orange]: reading.mmol_l >= 10,
+    [styles.bg_purple]: reading.mmol_l <= 4,
   })
 
 const displayClassNames = classNames({
@@ -29,13 +31,9 @@ const displayClassNames = classNames({
 
   return (
     <div className={counterClassNames}>
-      <FlapDisplay
-        className={displayClassNames}
-        chars={Presets.ALPHANUM + ".↗↘→↓↑⇈⇊"}
-        value={`${parseFloat(reading.mmol_l).toFixed(1)}${reading.trend_arrow}`}
-        length={reading.mmol_l >= 10 ? 5 : 4}
-        padMode={"start"}
-      />
+      <div className={displayClassNames}>
+          {parseFloat(reading.mmol_l).toFixed(1)}{reading.trend_arrow}
+      </div>
     </div>
   )
 }
