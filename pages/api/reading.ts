4c5bbb868fccc5fd91f898db0bdeb7407f7e97d1
David Li <david.li@cultureamp.com>
2023-09-05 14:22:48 +1000

    Push readings to db

--- pages/api/reading.ts
+++ pages/api/reading.ts
@@ -1,13 +1,35 @@
-import {NextApiRequest, NextApiResponse} from "next";
-import axios from "axios";
+import { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '@/app/lib/prisma'
+import axios from 'axios'
 
-const { DEXCOM_JSON_URL} = process.env
+const { DEXCOM_JSON_URL } = process.env
 export const getReadings = async (
   req: NextApiRequest,
   res: NextApiResponse,
 ) => {
   const results = await axios.get(`${DEXCOM_JSON_URL}`)
-  return res.status(200).json(results.data)
+  const { data } = results
+  return res.status(200).json(data)
+}
+
+export const addReading = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { mmol_l, trend_arrow, trend_description, datetime } = req.body
+    if (!mmol_l || !trend_arrow || !trend_description || !datetime) return {}
+
+    const result = await prisma.readings.create({
+      data: {
+        mmol_l,
+        trend_arrow,
+        trend_description,
+        datetime,
+      },
+    })
+    return res.status(200).json(result)
+  } catch (error) {
+    console.log('SQL ERROR: ', error)
+    return res.status(500).json({ error })
+  }
 }
 
 export default async function handler(
@@ -17,5 +39,7 @@ export default async function handler(
   switch (req.method) {
     case 'GET':
       return await getReadings(req, res)
+    case 'POST':
+      return await addReading(req, res)
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,44 @@
-import { useQuery } from '@tanstack/react-query'
-import classNames from "classnames"
-import styles from "./styles.module.scss"
+import { useMutation, useQuery } from '@tanstack/react-query'
+import classNames from 'classnames'
+import styles from './styles.module.scss'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Home() {
+  const [lastFetch, setLastFetch] = useState()
   const { data, isLoading } = useQuery(
     ['getReadings'],
-    async () =>
-      await fetch(`/api/reading`).then((res) =>
-        res.json(),
-      ),
-      {
-          refetchInterval: 6000,
-      }
+    async () => await fetch(`/api/reading`).then((res) => res.json()),
+    {
+      refetchInterval: 6000,
+    },
   )
 
-  if(isLoading || !data) return <div className={styles.container}>loading...</div>
+  const { reading } = data
 
-    const {reading} = data
+  const addReading = useMutation({
+    mutationFn: () =>
+      axios.post('/api/reading', {
+        mmol_l: reading.mmol_l,
+        trend_arrow: reading.trend_arrow,
+        trend_description: reading.trend_description,
+        datetime: new Date(reading.datetime).toISOString(),
+      }),
+    onSuccess: () => {
+      setLastFetch(reading.datetime)
+    },
+    onError: (error) => {
+      console.log('ERROR: ', error)
+    },
+  })
+
+  useEffect(() => {
+    console.log(data, lastFetch)
+    if (data && reading.datetime !== lastFetch) addReading.mutate()
+  }, [data])
+
+  if (isLoading || !data)
+    return <div className={styles.container}>loading...</div>
 
   const counterClassNames = classNames({
     [styles.container]: true,
@@ -25,16 +47,17 @@ export default function Home() {
     [styles.bg_purple]: reading.mmol_l <= 4,
   })
 
-const displayClassNames = classNames({
+  const displayClassNames = classNames({
     [styles.display]: true,
     [styles.display4]: reading.mmol_l < 10,
     [styles.display5]: reading.mmol_l >= 10,
-})
+  })
 
   return (
     <div className={counterClassNames}>
       <div className={displayClassNames}>
-          {parseFloat(reading.mmol_l).toFixed(1)}{reading.trend_arrow}
+        {parseFloat(reading.mmol_l).toFixed(1)}
+        {reading.trend_arrow}
       </div>
     </div>
   )
