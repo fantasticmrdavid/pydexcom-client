d58d9b997278b1d54714b29c49c66ba4abdbd0f7
David Li <fantasticmrdavid@gmail.com>
2023-09-19 19:28:05 +1000

    Add legacy browser support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  browserslist: [
+    '> 0.5%',
+    'not dead',
+    'ios >= 10',
+    'chrome >= 49',
+    'not ie <= 11',
+  ],
+}
 
 module.exports = nextConfig
