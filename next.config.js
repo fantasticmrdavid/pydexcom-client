3cffd246851eef731fecb5370555c629728e4987
David Li <fantasticmrdavid@gmail.com>
2023-09-25 16:46:32 +1000

    Add intl-relative-time-format polyfill

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  browserslist: [
-    '> 0.5%',
-    'not dead',
-    'ios >= 10',
-    'chrome >= 49',
-    'not ie <= 11',
-  ],
-}
+const nextConfig = {}
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import 'intl-relative-time-format'
 import '../styles/globals.css'
 
 import '@fontsource/roboto'
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
   return (
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
   )
 }
