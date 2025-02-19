f1a54f7a76d1ffdfe6e1ca908ef37eb096c00c89
David Li <fantasticmrdavid@gmail.com>
2025-02-19 23:52:06 +1100

    Remove queryclientprovider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import type { AppProps } from 'next/app'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import 'intl-relative-time-format'
 import '../styles/globals.css'
 import { Provider as ChakraUIProvider } from '@/components/ui/provider'
 
 import '@fontsource/roboto'
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(() => new QueryClient())
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraUIProvider forcedTheme={'light'}>
-        <Component {...pageProps} />
-      </ChakraUIProvider>
-    </QueryClientProvider>
+    <ChakraUIProvider forcedTheme={'light'}>
+      <Component {...pageProps} />
+    </ChakraUIProvider>
   )
 }
